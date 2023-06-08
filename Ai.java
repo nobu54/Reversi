2580271ebe4c6d8dd88d490283a43829510636b6
nobu <40336196+nobu54@users.noreply.github.com>
2023-06-08 09:00:08 +0900

    Ai.java を更新
    コメントを追加

--- Ai.java
+++ Ai.java
@@ -1,15 +1,28 @@
 import java.util.Random;
 
+/**
+* 対戦相手としてのCPUを実装するためのクラス
+* @parm level CPUの強さを表す値。0が最弱
+* @parm color CPUの色を表す値。0:黒、1:白
+*/
 public class Ai {
 
     int level;
     int color;
 
+    /**
+    * コンストラクタ
+    */
     public Ai(int lv, int cl) {
         this.level = lv;
         this.color = cl;
     }
 
+    /**
+    * 置く場所を決定し駒を置くメソッド
+    * @parm board
+    * @parm rule
+    */
     public void put(Board board, Rule rule) {
         
         // 置ける場所を取得
