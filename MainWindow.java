96d56292c5e90d2896a606cf96a0c18e5198624e
nobu <nobu1997gb@gmail.com>
2023-06-19 12:05:23 +0900

    文字列を変数で保存し、今後テキストの変更をしやすいようにした

--- MainWindow.java
+++ MainWindow.java
@@ -7,7 +7,7 @@ import javax.swing.border.TitledBorder;
 
 /**
 * このウィンドウでは、ゲームを始める際の設定を決める
-* 設定内容：CPU戦かPVPか,自分の色,置ける場所をハイライトするか否か
+* 設定内容：CPU戦かPVPか,自分(1p)の色,置ける場所をハイライトするか否か
 * 対戦相手 1p vs CPU or 1p vs 2p (radiogoup radioButton。デフォは1p vs cpu)
 * 自分の色　黒（先手） or 白（後手）　（radiogroup radiobutton デフォは1pが黒）
 * 置ける場所のハイライトのon off　（チェックボックス。チェックを入れるとon。デフォでonが良いかな）
@@ -22,11 +22,24 @@ public class MainWindow{
      * ウィンドウの作成
      */
     public MainWindow() {
+
+        // ウィンドウ内に表示するテキスト
+        String windowTitle = "ゲーム設定";
+        String battleModeText = "対戦形式";
+        String vsCpuText = "1p vs CPU";
+        String vsHumanText = "1p vs 2p";
+        String colorSelectText = "1Pの色";
+        String colorBlack = "黒         ";
+        String colorWhite = "白         ";
+        String isHighLightText = "<html>置ける場所を<br>ハイライト表示</html>";
+        String startText = "START";
+
+
         JFrame f = new JFrame();
         BoxLayout boxlayout = new BoxLayout(f.getContentPane(),BoxLayout.X_AXIS);
         f.setLayout(boxlayout);
-        f.setTitle("ゲーム設定");
-        f.setSize(300, 150);
+        f.setTitle(windowTitle);
+        f.setSize(320, 150);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Border lineBorder = BorderFactory.createLineBorder(Color.LIGHT_GRAY);
@@ -34,24 +47,24 @@ public class MainWindow{
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-        TitledBorder opponentBorder = BorderFactory.createTitledBorder(lineBorder, "対戦形式");
+        TitledBorder opponentBorder = BorderFactory.createTitledBorder(lineBorder, battleModeText);
         JPanel opponentPanel = new JPanel();
         opponentPanel.setLayout(new BoxLayout(opponentPanel, BoxLayout.PAGE_AXIS));
 		opponentPanel.setBorder(opponentBorder);
 
-        TitledBorder seleceColorBorder = BorderFactory.createTitledBorder(lineBorder, "自分の色");
+        TitledBorder seleceColorBorder = BorderFactory.createTitledBorder(lineBorder, colorSelectText);
         JPanel selectColorPanel = new JPanel();
         selectColorPanel.setLayout(new BoxLayout(selectColorPanel, BoxLayout.PAGE_AXIS));
         selectColorPanel.setBorder(seleceColorBorder);
 
         ButtonGroup bgroupOpponent = new ButtonGroup();
         ButtonGroup bgroupSelectColor = new ButtonGroup();
-        JRadioButton radioOpponent1 = new JRadioButton("1p vs CPU");
-        JRadioButton radioOpponent2 = new JRadioButton("1p vs 2p");
-        JCheckBox highlight = new JCheckBox("Highlight");
-        JRadioButton radioSelectBlack = new JRadioButton("黒       ");
-        JRadioButton radioSelectWhite = new JRadioButton("白       ");
-        JButton startButton = new JButton("START");
+        JRadioButton radioOpponent1 = new JRadioButton(vsCpuText);
+        JRadioButton radioOpponent2 = new JRadioButton(vsHumanText);
+        JCheckBox highlight = new JCheckBox(isHighLightText);
+        JRadioButton radioSelectBlack = new JRadioButton(colorBlack);
+        JRadioButton radioSelectWhite = new JRadioButton(colorWhite);
+        JButton startButton = new JButton(startText);
 
         bgroupOpponent.add(radioOpponent1);
         bgroupOpponent.add(radioOpponent2);
